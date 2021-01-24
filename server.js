bd5c2379b1b2d42dbd239098b7df50f10bbf3d6b
Ameed <ameedkabha@gmail.com>
2021-01-24 19:33:06 +0200

    q

--- server.js
+++ server.js
@@ -6,44 +6,11 @@ var bodyParser = require('body-parser')
 app.use(express.static('rct/build'));
 app.use(bodyParser.json())
 
-// const users = [
-//   { name: 'Kilani', password: '222222222' },
-//   { name: 'Yara', password: '333333333' },
-//   { name: 'Ihab', password: '444444' }
-// ]
-
-// //routes
-// app.get('/get-users', (req, res) => {
-
-//   res.send(users);
-// })
-
-// app.get('/get-password', (req, res) => {
-//   const { name } = req.query;
-
-//   console.log(req.query);
-
-//   let usr = users.find(user => user.name === name);
-//   console.log(usr)
-//   res.send({ password: usr.password })
-// })
-
-// app.post('/login', (req, res) => {
-
-//   const { name, password } = req.body;
-//   const index = users.findIndex(user => user.name === name);
-//   if (index === -1) {
-
-//     users.push({ name, password })
-//   }
-
-//   res.send({ ok: true })
-// })
 
 
 
-// const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
-// app.listen(port, function () {
-//   console.log('listening', port)
-// })
+app.listen(port, function () {
+  console.log('listening', port)
+})
